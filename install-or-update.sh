4a059ac4ef886167373093f21d4547b3d5c3841f
Bruno Polaco Penteado <polaco@gmail.com>
2012-10-27 14:41:49 -0200

    Improving installation script

--- install-or-update.sh
+++ install-or-update.sh
@@ -1,13 +1,18 @@
 cd $(dirname $0)
 
+# check for a git repo
+if [ ! -d .git ]; then git init; fi
+
 # adjust remotes
 git remote | xargs -I '$' git remote rm '$'
 git remote add -t master bcap git@github.com:bcap/oh-my-zsh.git
 git remote add -t master origin git://github.com/robbyrussell/oh-my-zsh.git
+git fetch --all
 
 # checkout and update master
-if ! git branch | grep -e '^* master$' > /dev/null; then git checkout master; fi
-git pull origin master
+git branch --set-upstream master origin/master
+git checkout master
+git pull
 
 # create the symbolic link
 if [ ! -e "$HOME/.zshrc" ]; then ln -s -v $(dirname $0)/.zshrc "$HOME/.zshrc"; fi
