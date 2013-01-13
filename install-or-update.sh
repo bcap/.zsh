a1b56e48f4517e5ee0923201ee2d798f96a3ee2b
Bruno Polaco Penteado <polaco@gmail.com>
2013-01-13 14:13:17 -0200

    Adding fork on read only mode for remote installations

--- install-or-update.sh
+++ install-or-update.sh
@@ -7,15 +7,17 @@ if [ ! -d .git ]; then git init; fi
 
 # adjust remotes
 git remote | xargs -I '$' git remote rm '$'
-git remote add -t master fork git@github.com:bcap/oh-my-zsh.git
+git remote add -t master fork   git@github.com:bcap/oh-my-zsh.git
+git remote add -t master forkro git://github.com/bcap/oh-my-zsh.git
 git remote add -t master origin git://github.com/robbyrussell/oh-my-zsh.git
-git fetch --all
+git fetch forkro
+git fetch origin
 
 # checkout and update master
 git branch --set-upstream master origin/master
 if ! git branch | grep -e '^* master$' > /dev/null; then git checkout master; fi
 git merge origin/master
-git merge fork/master
+git merge forkro/master
 
 # create the symbolic link
 if [ ! -e "$HOME/.zshrc" ]; then ln -s -v $(dirname $0)/.zshrc "$HOME/.zshrc"; fi
