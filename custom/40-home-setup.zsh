2f757eb2fa4c03c01cda4226b17b411cadb14991
Bruno Polaco <polaco@gmail.com>
2014-01-26 18:10:02 -0200

    Improving dropbox linking

--- custom/40-home-setup.zsh
+++ custom/40-home-setup.zsh
@@ -1,14 +1,16 @@
 # Dropbox linking
 if [[ -d Dropbox ]]; then
 
-    test ! -e .gnupg && ln -s Dropbox/nix/.gnupg
-    test ! -e .pip && ln -s Dropbox/nix/.pip
-    test ! -e .ssh && ln -s Dropbox/nix/.ssh
-    test ! -e bin && ln -s Dropbox/nix/bin
+    # create links in the $HOME pointing to everything at Dropbox/nix/home
+    for i in Dropbox/nix/home/{.,}*; do
+        test ! -e ${i##*/} && ln -vs $i
+    done
 
-    test ! -e .gitconfig && echo "warning: .gitconfig should be linked to a subitem in Dropbox/nix/git"
-
+    # special Sublime 2 link
     test ! -e "Library/Application Support/Sublime Text 2/Packages/User" &&
-        ln -s "../../../../Dropbox/softs/Sublime Text 2/User" "Library/Application Support/Sublime Text 2/Packages"
+        ln -vs "../../../../Dropbox/softs/Sublime Text 2/User" "Library/Application Support/Sublime Text 2/Packages"
+
+    # choosing which .gitconfig to use is a manual operation
+    test ! -e .gitconfig && echo "warning: .gitconfig should be linked to a subitem in Dropbox/nix/git"
 
 fi
