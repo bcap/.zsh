a908471340017aaacc1fb4adbd1f99c3e80a22d7
Bruno Polaco <polaco@gmail.com>
2014-04-06 15:09:50 -0300

    Minor functions to togle git check when evaluating the PS1 (for slow systems)

--- custom/20-functions.zsh
+++ custom/20-functions.zsh
@@ -19,3 +19,11 @@ run-if-exists() {
 sync-zsh() { rsync -av ~/.zsh ~/.zshrc "${1}:" }
 
 ssh-with-sync() { sync-zsh $1 && ssh $1 }
+
+toggle-git-on-ps1() {
+    value=$(git config --global --get oh-my-zsh.hide-status)
+
+    if [[ -z $value ]] || [[ $value == '0' ]]; then swapped=1 else swapped=0 fi
+
+    git config --global --replace-all oh-my-zsh.hide-status $swapped
+}
